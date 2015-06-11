880ff352968c7c10be868c6c9536a6f139b2fa73
Stephane Alnet <stephane@shimaore.net>
2015-06-11 11:54:42 +0200

    Proper build
    Not: we're only building tar files here, not Debian.

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-VERSION=0.1.4
+TARGET=fr/fr/sibylle
+VERSION=`jq -r .version package.json`
 (cd wav-$VERSION && ./build.sh)
 
-(perl map.pl map.txt)
+perl map.pl wav-$VERSION map.txt map2.txt
 
+cp README COPYRIGHT ${TARGET}
 for rate in 8000 16000 32000 48000; do
   echo "Building files for rate=$rate"
   find fr -wholename "fr/*/${rate}/*.wav" > list.txt
-  echo 'fr/fr/sibylle/README'    >> list.txt
-  echo 'fr/fr/sibylle/COPYRIGHT' >> list.txt
+  echo "${TARGET}/README"    >> list.txt
+  echo "${TARGET}/COPYRIGHT" >> list.txt
   tar czf freeswitch-sounds-fr-fr-sibylle-${rate}-${VERSION}.tar.gz -T list.txt --mode 0644 --owner root --group root -h
   rm list.txt
 done
